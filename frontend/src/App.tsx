6a9dda016e12c00f5dbe44125330012d443455a4
whatthefoobar <irina.mntn@gmail.com>
2023-05-30 10:40:26 +0200

    listing sample products

---
+++ frontend/src/App.tsx
@@ -0,0 +1,30 @@
+import "./App.css";
+import { sampleProducts } from "./data";
+
+function App() {
+  return (
+    <>
+      <div>
+        <header>TS Siren</header>
+        <main>
+          <ul>
+            {sampleProducts.map((product) => (
+              <li key={product.slug}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>${product.price}</p>
+              </li>
+            ))}
+          </ul>
+        </main>
+        <footer>All right reserved</footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
