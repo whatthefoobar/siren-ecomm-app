21e696e8d2303160278c38685df9cbd3c63a1122
whatthefoobar <irina.mntn@gmail.com>
2023-10-09 12:36:49 +0200

    trying to solve e.map is not a function

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -19,11 +19,12 @@ const HomePage = () => {
       <Helmet>
         <title>Siren</title>
       </Helmet>
-      {products!.map((product) => (
-        <Col key={product.slug} sm={6} md={4} lg={3}>
-          <ProductItem product={product} />
-        </Col>
-      ))}
+      {Array.isArray(products) &&
+        products.map((product) => (
+          <Col key={product.slug} sm={6} md={4} lg={3}>
+            <ProductItem product={product} />
+          </Col>
+        ))}
     </Row>
   );
 };
